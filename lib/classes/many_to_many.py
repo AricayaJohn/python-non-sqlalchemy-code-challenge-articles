328d21112289a7cf82e8b8e4bc410ff1b28da0dc
John <jaricaya@gmail.com>
2024-10-27 22:43:49 +0800

    aggregate and association Methods

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -4,6 +4,7 @@ class Article:
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
 
     @property
     def title(self):
@@ -64,10 +65,14 @@ class Author:
         return list({article.magazine for article in self.article()})
 
     def add_article(self, magazine, title):
-        pass
+        new_article = Article(self, magazine, title)
+        return new_article
 
     def topic_areas(self):
-        pass
+        article = self.article()
+        if not article:
+            return None
+        return list({article.magazine.category for article in article})
 
 class Magazine:
     def __init__(self, name, category):
@@ -112,7 +117,18 @@ class Magazine:
         return list({article.author for article in self.articles()})
 
     def article_titles(self):
-        pass
+        article = self.articles()
+        if not articles:
+            return None
+        return [article.title for article in articles]
 
     def contributing_authors(self):
-        pass
+        author_count = {}
+        for article in self.articles():
+            author = article.author
+            if author not in author_count:
+                author_count[author] = 0
+            author_count[author] += 1
+
+        contributing_authors = [author for author, count in author_count.items() if count > 2]
+        return contributing_authors if contributing_authors else None
